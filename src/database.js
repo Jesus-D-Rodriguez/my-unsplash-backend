fdf18189c3b9b4604be5fedbcac10f0af7522257
jesus <jdrch62@gmail.com>
2023-03-14 18:57:01 -0400

    Prueba 2

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const url = "mongodb+srv://my-unsplash-user:my-unsplash@my-unsplash.2gkamyo.mong
 
 const url2 = 'mongodb://127.0.0.1:27017/myunsplash';
 
-mongoose.connect(url2, {
+mongoose.connect(url, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
